31753f0e66b480e331940a54ebb8ff7389ce148d
Jenny Zhi <33742243+jenny7hi@users.noreply.github.com>
2019-09-19 08:53:38 -0700

    updating instructions

--- silly_biscuits.py
+++ silly_biscuits.py
@@ -7,7 +7,7 @@ def silly_biscuits():
 
 # sets up console window to appropriate size for printing game
 def setup_console():
-	cmd = 'mode 120,40'
+	cmd = 'mode 120,38'
 	os.system(cmd)
 
 # rules & prompts user for single vs. multiplayer and initializes game
@@ -15,15 +15,12 @@ def welcome_prompt():
 	print('Welcome to Silly Biscuits!\n')
 	print('RULES\nPlace cards greater than or less than the active card, depending on the direction.\nYou cannot play reserved cards until the deck is empty, and you cannot play hidden\nreserved cards until the public card above has been used. 2, 8, and 10 are special cards,\nas explained below. Objective is to play all your cards. ')
 	print('\nCOMMANDS')
-	print('\'[card]\' OR \'[card] from hand\': Plays specified card from hand.')
-	print('\'[card] from public\': Plays specified card from public reserved cards.')
-	print('\'[n] from hidden\': Plays nth hidden card.')
-	print('\'skip\': Skips your current turn.')
+	print('\'[card]\' OR \'[card] from hand\': plays specified card from hand')
+	print('\'[card] from public\': plays specified card from public reserved cards')
+	print('\'[n] from hidden\': plays nth hidden card')
+	print('\'skip\': skips your current turn')
 	print('\n*SPECIAL CARDS*')
-	print('A: Can be the biggest number if direction is going up and smallest number if direction is going down.')
-	print('2: Reverses direction, goes from last played card.')
-	print('8: Next player can play any card.')
-	print('10: Current player can play any card.')
+	print('A: biggest or smallest\t2: reverses direction\t8: next player plays any card\t10: current player plays any card')
 	print('\n-HIDDEN CARDS-')
 	print("Players do not know hidden cards before playing. If played and not legal, card is made public and turn is lost.")
 	print('\n\nHow many players are there? ')
